e3da547d780f3d5d4bcba2dadb7039fa2d360146
gregoryv <gregoryv@myumanitoba.ca>
2024-10-03 11:57:16 -0500

    Add migrations.py and have it skip mongo migrations

---
+++ cupboard_app/mirgrations.py
@@ -0,0 +1,7 @@
+# Ignore migrations for Mongo
+class Migration(migrations.Migration):
+    def apply(self, project_state, schema_editor, collect_sql=False):
+        pass
+
+    def unapply(self, project_state, schema_editor, collect_sql=False):
+        pass
