5e18d1ee06eedf92579bc9e10f98d0c73eda2329
gregoryv <gregoryv@myumanitoba.ca>
2024-10-04 15:05:30 -0500

    there's no such thing as min_length :(

--- cupboard_app/models.py
+++ cupboard_app/models.py
@@ -2,27 +2,27 @@ from django.db import models
 
 # Create your models here.
 class User(models.Model):
-    username = models.CharField(min_length=1,max_length=30,unique=True)
-    email = models.CharField(min_length=1,max_length=30,unique=True)
+    username = models.CharField(max_length=30,unique=True)
+    email = models.CharField(max_length=30,unique=True)
 
     def __str__(self):
         return f"{self.username} {self.email}"
     
 class Ingredient(models.Model):
-    name = models.CharField(min_length=1,max_length=25)
-    type = models.CharField(min_length=1,max_length=25)
+    name = models.CharField(max_length=25)
+    type = models.CharField(max_length=25)
 
     def __str__(self):
         return f"{self.name} {self.type}"
 
 class ListName(models.Model):
-    listName = models.CharField(min_length=1,max_length=15)
+    listName = models.CharField(max_length=15)
 
     def __str__(self):
         return f"{self.listName}"
 
 class Measurement(models.Model):
-    unit = models.CharField(min_length=1,max_length=15,unique=True)
+    unit = models.CharField(max_length=15,unique=True)
 
     def __str__(self):
         return f"{self.unit}"
@@ -37,7 +37,7 @@ class UserListIngredients(models.Model):
     
 class Recipe(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    recipeName = models.CharField(min_length=1,max_length=25)
+    recipeName = models.CharField(max_length=25)
     steps = []
     ingredients = []
 
