91feca28c1e19ab3657aedf1d91e28f82ed30134
Agape Seo <agapeseo@gmail.com>
2024-10-06 03:35:34 -0500

    com-93 add name to the urls

--- cupboard_app/urls.py
+++ cupboard_app/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('api/public', views.public),
-    path('api/private', views.private),
-    path('api/private-scoped', views.private_scoped),
+    path('api/public', views.public, name='public'),
+    path('api/private', views.private, name='private'),
+    path('api/private-scoped', views.private_scoped, name='private-scoped'),
 ]
