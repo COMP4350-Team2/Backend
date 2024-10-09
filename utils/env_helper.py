160e0ba27e352ff4445dfca60c50664a4590d630
Agape Seo <agapeseo@gmail.com>
2024-10-09 12:39:04 -0500

    com-111 update environment variables to use default if missing or empty string

--- utils/env_helper.py
+++ utils/env_helper.py
@@ -21,19 +21,64 @@ def load_env_variables():
 
     # Set defaults for all the environment variables in case user doesn't have
     # .env file
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cupboard_backend.settings')
-    os.environ.setdefault('DJANGO_SECRET_KEY', 'django-insecure-{}'.format(generate_secret()))
-    os.environ.setdefault('DJANGO_PORT', '6060')
-    os.environ.setdefault('REACT_CLIENT_ORIGIN_URL', 'http://localhost:8080')
-    os.environ.setdefault('CUPBOARD_TEST_CLIENT_ID', '')
-    os.environ.setdefault('CUPBOARD_TEST_CLIENT_SECRET', '')
-    os.environ.setdefault('DEV_LAYER', 'mock')
-    os.environ.setdefault('DEBUG_ENABLE', 'true')
-    os.environ.setdefault('TEST_RUN', 'true')
-    os.environ.setdefault('TEST_KEY', 'cupboard_secret')
-    os.environ.setdefault('AUTHLIB_INSECURE_TRANSPORT', 'true')
-    os.environ.setdefault('AUTH0_DOMAIN', 'my-domain.ca.auth0.com')
-    os.environ.setdefault('AUTH0_API_IDENTIFIER', 'https://api.example.com')
-    os.environ.setdefault('DB_NAME', 'DCupboardDB')
-    os.environ.setdefault('DB_TEST_NAME', 'test_DCupboardDB')
-    os.environ.setdefault('MONGO_URL', '')
+    if (
+        os.getenv('DJANGO_SETTINGS_MODULE') == ''
+        or os.getenv('DJANGO_SETTINGS_MODULE') is None
+    ):
+        os.environ['DJANGO_SETTINGS_MODULE'] = 'cupboard_backend.settings'
+
+    if (
+        os.getenv('DJANGO_SECRET_KEY') == ''
+        or os.getenv('DJANGO_SECRET_KEY') is None
+    ):
+        os.environ['DJANGO_SECRET_KEY'] = 'django-insecure-{}'.format(generate_secret())
+
+    if os.getenv('DJANGO_PORT') == '' or os.getenv('DJANGO_PORT') is None:
+        os.environ['DJANGO_PORT'] = '6060'
+
+    if os.getenv('REACT_CLIENT_ORIGIN_URL') == '' or os.getenv('REACT_CLIENT_ORIGIN_URL') is None:
+        os.environ['REACT_CLIENT_ORIGIN_URL'] = 'http://localhost:8080'
+
+    if os.getenv('CUPBOARD_TEST_CLIENT_ID') == '' or os.getenv('CUPBOARD_TEST_CLIENT_ID') is None:
+        os.environ['CUPBOARD_TEST_CLIENT_ID'] = ''
+
+    if (
+        os.getenv('CUPBOARD_TEST_CLIENT_SECRET') == ''
+        or os.getenv('CUPBOARD_TEST_CLIENT_SECRET') is None
+    ):
+        os.environ['CUPBOARD_TEST_CLIENT_SECRET'] = ''
+
+    if os.getenv('DEV_LAYER') == '' or os.getenv('DEV_LAYER') is None:
+        os.environ['DEV_LAYER'] = 'mock'
+
+    if os.getenv('DEBUG_ENABLE') == '' or os.getenv('DEBUG_ENABLE') is None:
+        os.environ['DEBUG_ENABLE'] = 'true'
+
+    if os.getenv('TEST_RUN') == '' or os.getenv('TEST_RUN') is None:
+        os.environ['TEST_RUN'] = 'true'
+
+    if os.getenv('TEST_KEY') == '' or os.getenv('TEST_KEY') is None:
+        os.environ['TEST_KEY'] = 'cupboard_secret'
+
+    if (
+        os.getenv('AUTHLIB_INSECURE_TRANSPORT') == ''
+        or os.getenv('AUTHLIB_INSECURE_TRANSPORT') is None
+    ):
+        os.environ['AUTHLIB_INSECURE_TRANSPORT'] = 'true'
+
+    if os.getenv('AUTH0_DOMAIN') == '' or os.getenv('AUTH0_DOMAIN') is None:
+        os.environ['AUTH0_DOMAIN'] = 'my-domain.ca.auth0.com'
+
+    if os.getenv('AUTH0_API_IDENTIFIER') == '' or os.getenv('AUTH0_API_IDENTIFIER') is None:
+        os.environ['AUTH0_API_IDENTIFIER'] = 'https://api.example.com'
+
+    if os.getenv('DB_NAME') == '' or os.getenv('DB_NAME') is None:
+        os.environ['DB_NAME'] = 'DCupboardDB'
+
+    if os.getenv('DB_TEST_NAME') == '' or os.getenv('DB_TEST_NAME') is None:
+        os.environ['DB_TEST_NAME'] = 'test_DCupboardDB'
+
+    if os.getenv('MONGO_URL') == '' or os.getenv('MONGO_URL') is None:
+        os.environ['MONGO_URL'] = ''
+
+    print('DB_NAME: "{}"'.format(os.getenv('DB_NAME')))
