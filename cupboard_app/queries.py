066349abcc7d39e781a4944ff24e2bb7998a6b76
gregoryv <gregoryv@myumanitoba.ca>
2024-10-19 22:37:08 -0500

    com-134, com-128 Add query that updates and adds ingredients to user lists

--- cupboard_app/queries.py
+++ cupboard_app/queries.py
@@ -290,3 +290,66 @@ def get_user_lists_ingredients(username: str, id: int = None) -> QuerySet:
         )
 
     return result
+
+def update_list_ingredient(
+    username: str,
+    listName: str,
+    ingredient: str,
+    amount: int | float,
+    unit: str
+) -> str:
+    """
+    Updates an ingredient in the user's list.
+
+    If ingredient exists in the user's list, then update the given ingredient's
+    amount and unit in the passed amount and unit.
+
+    If ingredient does not exist in the user's list, then add new ingredient
+    dictionary to user's list.
+
+    Args:
+        username: User's username
+        listName: Name of the list
+        ingredient: Name of the ingredient
+        amount: Quantity of the ingredient
+        unit: The unit of measure for the ingredient
+    """
+    result = UPDATE_FAILED_MSG
+    try:
+        list_ingredient = create_list_ingredient(
+            ingredient=ingredient,
+            amount=amount,
+            unit=unit
+        )
+        if list_ingredient:
+            # Check list exists
+            user_list = UserListIngredients.objects.filter(
+                user__username=username,
+                listName__listName=listName
+            ).first()
+            if user_list:
+                search_id = list_ingredient.get('ingredientId')
+                unit_id = list_ingredient.get('unitId')
+                # Check if ingredient exists
+                if not any(
+                    dictionary.get('ingredientId', None) == search_id
+                    for dictionary in user_list.ingredients
+                ):
+                    # ingredient does not exist so insert
+                    user_list.ingredients.append(list_ingredient)
+                else:
+                    # ingredient exists so update ingredient
+                    for i in user_list.ingredients:
+                        if i['ingredientId'] == search_id:
+                            i['amount'] = amount
+                            i['unitId'] = unit_id
+                user_list.save()
+                result = UPDATE_SUCCESS_MSG
+            else:
+                result = DOES_NOT_EXIST_MSG
+        else:
+            result = f'{UPDATE_FAILED_MSG} Ingredient does not exist.'
+    except Exception:
+        result = UPDATE_FAILED_MSG
+
+    return result
