aca9cd039063e2e7cfbbf238b78fe1e56c7888e8
gregoryv <gregoryv@myumanitoba.ca>
2024-11-03 12:19:35 -0600

    com-134 remove src folder

--- utils/db_helper/add_ingredients.py
+++ utils/db_helper/add_ingredients.py
@@ -1,14 +1,18 @@
 import json
+import os
 from cupboard_app.queries import (
-    create_ingredient,
+    create_ingredient
 )
 
-filepath = '/src/Food.json'
+
+filepath = os.getenv('JSON_PATH')
 with open(filepath, 'r', encoding="utf8") as file:
     data = json.load(file)
 
-for i in data:
-    if i["food_group"] is not None:
-        create_ingredient(i["name"], i["food_group"])
-    else:
-        create_ingredient(i["name"], "Miscellaneous")
+    for i in data:
+        if i["food_group"] is not None:
+            create_ingredient(i["name"], i["food_group"])
+        else:
+            create_ingredient(i["name"], "Miscellaneous")
+
+
